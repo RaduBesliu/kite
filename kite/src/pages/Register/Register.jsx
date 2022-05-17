e7e6dbb2ec8a8e495f51b9ff5de936268d36bf36
Radu <radu.besliu02@gmail.com>
2022-05-17 20:01:47 +0300

    redo imports + verify inputs + add validate_error

--- kite/src/pages/Register/Register.jsx
+++ kite/src/pages/Register/Register.jsx
@@ -1,26 +1,40 @@
+// React
 import React from "react";
 import { useState, useEffect } from "react";
 
-import { useNavigate } from "react-router-dom";
-
+// Libraries
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 
+// Components
 import AccountInput from "../../components/AccountInput";
 import Button from "../../components/Button";
 
+// Helpers
+import { validate_error } from "../../helpers/validate_error";
+
+// Styles
 import "./register.css";
 
 function register() {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [select, setSelect] = useState("");
 
   const [token, setToken] = useState(localStorage.getItem("token"));
 
-  const [select, setSelect] = useState("");
+  const [isValidName, setIsValidName] = useState(null);
+  const [isValidUsername, setIsValidUsername] = useState(null);
+  const [isValidPassword, setIsValidPassword] = useState(null);
 
   const navigate = useNavigate();
   const BASE_URL = process.env.REACT_APP_API_ENDPOINT;
+
+  const emailRegex = RegExp(
+    "^[a-z0-9-_](?:[a-z0-9-_.]*)?[a-z0-9-_]?@[a-z0-9](?:[a-z0-9-.]*)?[a-z0-9]?$",
+    "gi"
+  );
 
   // Redirect home if there is an auth token
   useEffect(() => {
@@ -31,11 +45,40 @@ function register() {
 
   const submitForm = (e) => {
     e.preventDefault();
-    console.log({ name: select + name, username, password });
-    // axios.post(`${BASE_URL}/login`, { username, password }).then((response) => {
-    //   localStorage.setItem("token", response.data.userId);
-    //   setToken(response.data.userId);
-    // });
+
+    // Check all inputs first for error, then return if invalid
+    let validForm = true;
+
+    if (!name) {
+      setIsValidName(false);
+      validForm = false;
+    } else setIsValidName(true);
+
+    if (!(username && emailRegex.test(username))) {
+      setIsValidUsername(false);
+      validForm = false;
+    } else setIsValidUsername(true);
+
+    if (!password) {
+      setIsValidPassword(false);
+      validForm = false;
+    } else setIsValidPassword(true);
+
+    if (!validForm) return;
+
+    // After registration, login user automatically
+    axios
+      .post(`${BASE_URL}/user`, { name: select + name, email: username })
+      .then(() => {
+        axios
+          .post(`${BASE_URL}/login`, { username, password })
+          .then((response) => {
+            localStorage.setItem("token", response.data.userId);
+            setToken(response.data.userId);
+          })
+          .catch((err) => validate_error(err));
+      })
+      .catch((err) => validate_error(err));
   };
 
   return (
@@ -51,12 +94,14 @@ function register() {
           <option value="Mr. ">Mr.</option>
           <option value="Mrs. ">Mrs.</option>
           <option value="Ms. ">Ms.</option>
+          <option value="Miss ">Miss</option>
         </select>
         <AccountInput
           title="Name"
           type="text"
           value={name}
           setValue={setName}
+          isValid={isValidName}
         ></AccountInput>
       </div>
       <AccountInput
@@ -64,18 +109,42 @@ function register() {
         type="text"
         value={username}
         setValue={setUsername}
+        isValid={isValidUsername}
       ></AccountInput>
       <AccountInput
         title="Password"
         type="password"
         value={password}
         setValue={setPassword}
+        isValid={isValidPassword}
       ></AccountInput>
       <Button
         className="form__submit"
         title="Register"
         handleClick={submitForm}
       />
+      <p
+        style={{
+          fontSize: 20,
+          display: "block",
+          textAlign: "center",
+          marginTop: 16,
+          letterSpacing: 1.15,
+        }}
+      >
+        Already have an account?
+        <br />
+        <Link
+          to="../login"
+          style={{
+            textDecoration: "none",
+            color: "var(--clr-button-primary)",
+            fontWeight: "bold",
+          }}
+        >
+          Login
+        </Link>
+      </p>
     </form>
   );
 }
