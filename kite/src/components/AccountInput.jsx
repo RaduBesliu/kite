3102f5e007fa1eb9b2aa538a6c32b395b3e8d4a3
Radu <radu.besliu02@gmail.com>
2022-05-17 19:59:51 +0300

    add error border color

--- kite/src/components/AccountInput.jsx
+++ kite/src/components/AccountInput.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-function AccountInput({ type, title, value, setValue }) {
+function AccountInput({ type, title, value, setValue, isValid }) {
   return (
     <div className="input--flex">
       <label className="input__label">{title}</label>
       <input
-        className="input__data"
+        className={`input__data ${
+          isValid === false ? "input__data--error" : ""
+        }`}
         type={type}
         value={value}
         onChange={(e) => setValue(e.target.value)}
