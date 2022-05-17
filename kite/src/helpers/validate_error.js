f45b206de24346ffe657d52b44da3296803095d0
Radu <radu.besliu02@gmail.com>
2022-05-17 19:58:19 +0300

    added function to validate errors

---
+++ kite/src/helpers/validate_error.js
@@ -0,0 +1,9 @@
+export function validate_error(err) {
+  if (err.response) {
+    console.log(err.response.data);
+  } else if (err.request) {
+    console.log(err.request);
+  } else {
+    console.log("Error", err.message);
+  }
+}
