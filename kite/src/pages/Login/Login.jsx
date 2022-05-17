159cdd24cc9afe606300bc2e5bcf5c33167f234c
Radu <radu.besliu02@gmail.com>
2022-05-17 20:01:17 +0300

    redo imports + link to register + add validate_error

--- kite/src/pages/Login/Login.jsx
+++ kite/src/pages/Login/Login.jsx
@@ -1,13 +1,19 @@
+// React
 import React from "react";
 import { useState, useEffect } from "react";
 
-import { useNavigate } from "react-router-dom";
-
+// Libraries
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 
+// Componets
 import AccountInput from "../../components/AccountInput";
 import Button from "../../components/Button";
 
+// Helpers
+import { validate_error } from "../../helpers/validate_error";
+
+// Styles
 import "./login.css";
 
 function login() {
@@ -27,10 +33,14 @@ function login() {
 
   const submitForm = (e) => {
     e.preventDefault();
-    axios.post(`${BASE_URL}/login`, { username, password }).then((response) => {
-      localStorage.setItem("token", response.data.userId);
-      setToken(response.data.userId);
-    });
+
+    axios
+      .post(`${BASE_URL}/login`, { username, password })
+      .then((response) => {
+        localStorage.setItem("token", response.data.userId);
+        setToken(response.data.userId);
+      })
+      .catch((err) => validate_error(err));
   };
 
   return (
@@ -49,6 +59,28 @@ function login() {
         setValue={setPassword}
       ></AccountInput>
       <Button className="form__submit" title="Login" handleClick={submitForm} />
+      <p
+        style={{
+          fontSize: 20,
+          display: "block",
+          textAlign: "center",
+          marginTop: 16,
+          letterSpacing: 1.15,
+        }}
+      >
+        Don't have an account?
+        <br />
+        <Link
+          to="../register"
+          style={{
+            textDecoration: "none",
+            color: "var(--clr-button-primary)",
+            fontWeight: "bold",
+          }}
+        >
+          Register
+        </Link>
+      </p>
     </form>
   );
 }
