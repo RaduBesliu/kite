5cb503861e24df44ec1b9f7b0dd39387065f55f5
Radu <radu.besliu02@gmail.com>
2022-05-17 20:19:00 +0300

    comment proper auth => add fake auth

--- kite/src/auth/PrivateRoute.jsx
+++ kite/src/auth/PrivateRoute.jsx
@@ -4,33 +4,44 @@ import { useEffect, useState } from "react";
 
 // Libraries
 import { Navigate, Outlet } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 
 // Helpers
-import { validate_error } from "../helpers/validate_error";
+// import { validate_error } from "../helpers/validate_error";
 
 function PrivateRoute() {
   const [auth, setAuth] = useState(true);
 
-  const BASE_URL = process.env.REACT_APP_API_ENDPOINT;
   const userToken = localStorage.getItem("token");
 
-  // If the user doesn't exist in the database, then logout
-  useEffect(() => {
-    const fetchData = async () => {
-      axios
-        .get(`${BASE_URL}/user/${userToken}`)
-        .then(() => {
-          setAuth(true);
-        })
-        .catch((err) => {
-          setAuth(false);
-          localStorage.removeItem("token");
-          validate_error(err);
-        });
-    };
+  // This will be used if there is proper authentication
+  // If the user id isn't in the database, the user is logged out and local storage token is cleared
+  // Prevents users from entering random tokens
 
-    fetchData();
+  // const BASE_URL = process.env.REACT_APP_API_ENDPOINT;
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     axios
+  //       .get(`${BASE_URL}/user/${userToken}`)
+  //       .then(() => {
+  //         setAuth(true);
+  //       })
+  //       .catch((err) => {
+  //         setAuth(false);
+  //         localStorage.removeItem("token");
+  //         validate_error(err);
+  //       });
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  // If there is no token present, logout
+  useEffect(() => {
+    if (!userToken) {
+      localStorage.removeItem("token");
+      setAuth(false);
+    }
   }, []);
 
   return auth ? <Outlet /> : <Navigate to="./login" />;
