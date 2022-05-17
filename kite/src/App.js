3c354916451333c692c6bacb5e4d3cda1f7a3f5d
Radu <radu.besliu02@gmail.com>
2022-05-17 20:18:27 +0300

    add global styles

--- kite/src/App.js
+++ kite/src/App.js
@@ -1,18 +1,23 @@
+// React
 import React from "react";
 
+// Libraries
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// Components
 import PrivateRoute from "./auth/PrivateRoute";
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Error from "./pages/Error/Error";
 
-import "./app.css";
+// Styles
+import { GlobalStyles } from "./GlobalStyles.styles";
 
 function App() {
   return (
     <Router>
+      <GlobalStyles />
       <Routes>
         <Route exact path="/" element={<PrivateRoute />}>
           <Route exact path="/" element={<Home />} />
