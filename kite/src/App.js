385aea1f33cc690a0e3009faed4264e5dbda7ab5
Radu <radu.besliu02@gmail.com>
2022-05-18 03:22:06 +0300

    add global styles

--- kite/src/App.js
+++ kite/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 
 // Libraries
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 // Components
 import PrivateRoute from "./auth/PrivateRoute";
@@ -18,6 +19,12 @@ function App() {
   return (
     <Router>
       <GlobalStyles />
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Calligraffitti&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Routes>
         <Route exact path="/" element={<PrivateRoute />}>
           <Route exact path="/" element={<Home />} />
