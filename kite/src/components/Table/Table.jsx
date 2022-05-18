dafd7e0214fef7f317c0d4cf01f432a690a3e456
Radu <radu.besliu02@gmail.com>
2022-05-19 02:19:08 +0300

    update Table component + styles

--- kite/src/components/Table/Table.jsx
+++ kite/src/components/Table/Table.jsx
@@ -1,11 +1,69 @@
+// React
 import React from "react";
 
-function Table() {
+// Libraries
+import { BsArrowDownUp } from "react-icons/bs";
+
+function Table({ className, shownSpots }) {
   return (
-    <table>
-      <th>
-        <td>Test</td>
-      </th>
+    <table className={className}>
+      <thead>
+        <tr>
+          <th>
+            <div className="flex--container">
+              <span>Name</span>
+              <BsArrowDownUp />
+            </div>
+          </th>
+          <th>
+            {" "}
+            <div className="flex--container">
+              <span>Country</span>
+              <BsArrowDownUp />
+            </div>
+          </th>
+          <th>
+            {" "}
+            <div className="flex--container">
+              <span>Latitude</span>
+              <BsArrowDownUp />
+            </div>
+          </th>
+          <th>
+            {" "}
+            <div className="flex--container">
+              <span>Longitude</span>
+              <BsArrowDownUp />
+            </div>
+          </th>
+          <th>
+            {" "}
+            <div className="flex--container">
+              <span>Wind Prob.</span>
+              <BsArrowDownUp />
+            </div>
+          </th>
+          <th>
+            {" "}
+            <div className="flex--container">
+              <span>When to go</span>
+              <BsArrowDownUp />
+            </div>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {shownSpots.map((spot) => (
+          <tr key={spot.id}>
+            <td>{spot.name}</td>
+            <td>{spot.country}</td>
+            <td>{spot.lat}</td>
+            <td>{spot.long}</td>
+            <td>{spot.probability}%</td>
+            <td>{spot.month}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
--- kite/src/components/Table/Table.styles.jsx
+++ kite/src/components/Table/Table.styles.jsx
@@ -4,7 +4,49 @@ import styled from "styled-components";
 import Table from "./Table";
 
 export const StyledTable = styled(Table)`
-  background-color: red;
-  height: 100px;
-  width: 100px;
+  width: 100%;
+  display: block;
+  overflow-x: auto;
+  /* white-space: nowrap; */
+  border-spacing: 0;
+  font-size: 20px;
+  margin-bottom: 100px;
+
+  & th {
+    text-align: left;
+    height: 50px;
+    border-right: 19px solid transparent;
+    padding-left: 5px;
+    font-weight: 500;
+  }
+
+  & td {
+    width: calc(100% / 7);
+    height: 50px;
+    padding-left: 5px;
+    border-right: 19px solid transparent;
+  }
+
+  & .flex--container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-width: 175px;
+  }
+
+  & thead tr {
+    background-color: var(--clr-dark-gray);
+  }
+
+  & tbody tr:nth-child(2n) {
+    background-color: var(--clr-light-gray);
+  }
+
+  @media (max-width: 1500px) {
+    font-size: 18px;
+  }
+
+  @media (max-width: 550px) {
+    font-size: 16px;
+  }
 `;
