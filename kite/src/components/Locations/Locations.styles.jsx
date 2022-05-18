d4090b10b7c533cd600ce591dca9f6e44c7b8c5c
Radu <radu.besliu02@gmail.com>
2022-05-19 02:13:06 +0300

    update Locations styles

--- kite/src/components/Locations/Locations.styles.jsx
+++ kite/src/components/Locations/Locations.styles.jsx
@@ -2,4 +2,16 @@ import styled from "styled-components";
 
 import Locations from "./Locations";
 
-export const StyledLocations = styled(Locations)``;
+export const StyledLocations = styled(Locations)`
+  width: 85%;
+  margin-inline: auto;
+  margin-top: 50px;
+
+  & h1 {
+    margin-bottom: 9px;
+  }
+
+  & h2 {
+    margin-bottom: 100px;
+  }
+`;
