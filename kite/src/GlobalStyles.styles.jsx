3fc1f1ffa64a0dd5ca8f20f2e95c4562d6bed2e1
Radu <radu.besliu02@gmail.com>
2022-05-18 03:22:33 +0300

    add global resets + variables

--- kite/src/GlobalStyles.styles.jsx
+++ kite/src/GlobalStyles.styles.jsx
@@ -12,7 +12,6 @@ export const GlobalStyles = createGlobalStyle`
 
   :root {
     --clr-white: white;
-    --clr-background: #f2f2f2;
     --clr-light-gray: #f3f3f3;
     --clr-mid-gray: #e4e7e9;
     --clr-dark-gray: #979797;
@@ -20,8 +19,9 @@ export const GlobalStyles = createGlobalStyle`
     --clr-font-secondary: #787878;
     --clr-button-primary: #4a90e2;
     --clr-button-secondary: #007aff;
+    --clr-button-third: #fafafa;
     --clr-red: #ff3b30;
-    --clr-orange: #e8b100;
+    --clr-yellow: #e8b100;
 
     --font-calli: "Calligraffitti", sans-serif;
   }
