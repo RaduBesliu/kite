f960ef861701ff2a74e5c4323477c7a2e24ee5db
Radu <radu.besliu02@gmail.com>
2022-05-20 03:18:15 +0300

    move filters button and form here

--- kite/src/pages/Home/Home.jsx
+++ kite/src/pages/Home/Home.jsx
@@ -8,16 +8,24 @@ import { validate_error } from "../../helpers/validate_error";
 // Libraries
 import { Helmet } from "react-helmet";
 import axios from "axios";
+import { BiFilter } from "react-icons/bi";
+import { useSelector } from "react-redux";
 
 // Components
 import { StyledMap } from "../../components/Map/Map.styles";
 import { StyledNavbar } from "../../components/Navbar/Navbar.styles";
 import { StyledLocations } from "../../components/Locations/Locations.styles";
+import { StyledFilterForm } from "../../components/FilterForm/FilterForm.styles";
+import { StyledButton } from "../../components/Button/Button.styles";
 
 function home() {
   const [spots, setSpots] = useState([]);
+  const [filterSpots, setFilterSpots] = useState([]);
+  const [showFilterForm, setShowFilterForm] = useState(false);
 
   const BASE_URL = process.env.REACT_APP_API_ENDPOINT;
+
+  const filter = useSelector((state) => state.filter.value);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,11 +33,24 @@ function home() {
         .get(`${BASE_URL}/spot`)
         .then((response) => response.data)
         .catch((err) => validate_error(err));
+      setFilterSpots(data);
       setSpots(data);
     };
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    setFilterSpots(
+      spots.filter(
+        (spot) =>
+          (!filter.country ||
+            spot.country.toLowerCase().includes(filter.country)) &&
+          (!filter.windProbability ||
+            spot.probability === parseInt(filter.windProbability))
+      )
+    );
+  }, [filter, spots]);
 
   return (
     <>
@@ -48,11 +69,43 @@ function home() {
         <title>Kite | Locations</title>
       </Helmet>
       <StyledNavbar />
-      <StyledMap spots={spots} />
-      {!spots.length ? (
+      <StyledMap filterSpots={filterSpots} />
+      <StyledButton
+        width="150px"
+        height="44px"
+        backgroundColor="white"
+        position="absolute"
+        zIndex="400"
+        color="var(--clr-font-primary)"
+        top={"135px"}
+        right={"18px"}
+        filter={"drop-shadow(0px 2px 4px rgba(0, 0, 0,  0.5))"}
+        handleClick={() => setShowFilterForm(true)}
+        buttonLabel={
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "7px",
+            }}
+          >
+            <BiFilter size={22} />
+            {"FILTERS"}
+          </div>
+        }
+      />
+      {showFilterForm && (
+        <StyledFilterForm
+          boxShadow="0px 0px 8px rgba(0, 0, 0, 0.12), 0px 8px 8px rgba(0, 0, 0, 0.24)"
+          borderRadius="2px"
+          setShowFilterForm={setShowFilterForm}
+        />
+      )}
+      {!filterSpots.length ? (
         <h1>Loading spots...</h1>
       ) : (
-        <StyledLocations spots={spots} />
+        <StyledLocations filterSpots={filterSpots} />
       )}
     </>
   );
