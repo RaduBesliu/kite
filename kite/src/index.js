b56524e578a82658d27dd4f1741788c1a49bd685
Radu <radu.besliu02@gmail.com>
2022-05-20 03:17:39 +0300

    add redux store and provider

--- kite/src/index.js
+++ kite/src/index.js
@@ -1,10 +1,19 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Libraries
+import { Provider } from "react-redux";
+
+// Store
+import { store } from "./store";
+
+// Components
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
