d2b3a69f26beefda62b418cf6fca357f26bb59bc
Radu <radu.besliu02@gmail.com>
2022-05-20 03:10:02 +0300

    create layout + update global filter state

--- kite/src/components/FilterForm/FilterForm.jsx
+++ kite/src/components/FilterForm/FilterForm.jsx
@@ -1,22 +1,64 @@
+// React
 import React from "react";
+import { useState } from "react";
+
+// Libraries
+import { useDispatch } from "react-redux";
 
 // Components
 import { StyledButton } from "../Button/Button.styles";
 import { StyledFilterFormInput } from "./FilterFormInput/FilterFormInput.styles";
 
-function FilterForm({ className }) {
+// Features
+import { setFilter } from "../../features/filter/filterSlice";
+
+function FilterForm({ className, setShowFilterForm }) {
+  const [formData, setFormData] = useState({
+    country: "",
+    windProbability: "",
+  });
+
+  const dispatch = useDispatch();
+
+  const submitFilterForm = (e) => {
+    e.preventDefault();
+
+    dispatch(setFilter(formData));
+    setShowFilterForm(false);
+  };
+
   return (
     <form className={className}>
       <StyledFilterFormInput
+        className={"form__input"}
         width="217px"
-        border="2.38px solid var(--clr-input-border)"
+        height="30px"
+        name="country"
+        borderBottom="2.38px solid var(--clr-input-border)"
+        backgroundColor="var(--clr-button-third)"
+        labelText="Country"
+        formData={formData}
+        setFormData={setFormData}
+      />{" "}
+      <StyledFilterFormInput
+        className={"form__input"}
+        width="217px"
+        height="30px"
+        name="windProbability"
+        borderBottom="2.38px solid var(--clr-input-border)"
+        backgroundColor="var(--clr-button-third)"
+        labelText="Wind Probability"
+        formData={formData}
+        setFormData={setFormData}
       />
       <StyledButton
+        className={"form__submit"}
         width="150px"
         height="44px"
         buttonLabel={"APPLY FILTER"}
         color="var(--clr-font-primary)"
         filter="drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.5))"
+        handleClick={submitFilterForm}
       />
     </form>
   );
