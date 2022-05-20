6851f9fe8c94cb19e88381a46fe8e58ee08b7200
Radu <radu.besliu02@gmail.com>
2022-05-20 03:16:24 +0300

    add option to sort by every column + specific icons

--- kite/src/components/Table/Table.jsx
+++ kite/src/components/Table/Table.jsx
@@ -1,59 +1,94 @@
 // React
 import React from "react";
+import { useState } from "react";
 
-// Libraries
-import { BsArrowDownUp } from "react-icons/bs";
+// Components
+import TableHeader from "./TableHeader";
 
 function Table({ className, shownSpots }) {
+  const [sortParams, setSortParams] = useState({
+    sortKey: "",
+    sortDirection: 0,
+  });
+
+  const numbers = ["lat", "long", "probability"];
+
+  const tableHeadValues = [
+    { name: "Name", sortKey: "name" },
+    { name: "Country", sortKey: "country" },
+    { name: "Latitude", sortKey: "lat" },
+    { name: "Longitude", sortKey: "long" },
+    { name: "Wind Prob.", sortKey: "probability" },
+    { name: "When to go", sortKey: "month" },
+  ];
+
+  const monthsOrder = {
+    January: 1,
+    February: 2,
+    March: 3,
+    April: 4,
+    May: 5,
+    June: 6,
+    July: 7,
+    August: 8,
+    September: 9,
+    October: 10,
+    November: 11,
+    December: 12,
+  };
+
+  // Returns sorted array depending on the column
+  const sortedSpots = () => {
+    const sortKey = sortParams.sortKey;
+    const sortDirection = sortParams.sortDirection;
+
+    if (!sortKey || !sortDirection) return shownSpots;
+
+    // Sort by month index, not by name
+    if (sortKey === "month") {
+      return [...shownSpots].sort(
+        (spot1, spot2) =>
+          sortDirection *
+          (parseFloat(monthsOrder[spot1[sortKey]]) -
+            parseFloat(monthsOrder[spot2[sortKey]]))
+      );
+    }
+
+    // Sort number columns with parseFloat
+    if (numbers.some((number) => number === sortKey)) {
+      return [...shownSpots].sort(
+        (spot1, spot2) =>
+          sortDirection *
+          (parseFloat(spot1[sortKey]) - parseFloat(spot2[sortKey]))
+      );
+    }
+
+    // Sort string columns with compare
+    else {
+      return [...shownSpots].sort(
+        (spot1, spot2) =>
+          sortDirection * spot1[sortKey].localeCompare(spot2[sortKey])
+      );
+    }
+  };
+
   return (
     <table className={className}>
       <thead>
         <tr>
-          <th>
-            <div className="flex--container">
-              <span>Name</span>
-              <BsArrowDownUp />
-            </div>
-          </th>
-          <th>
-            {" "}
-            <div className="flex--container">
-              <span>Country</span>
-              <BsArrowDownUp />
-            </div>
-          </th>
-          <th>
-            {" "}
-            <div className="flex--container">
-              <span>Latitude</span>
-              <BsArrowDownUp />
-            </div>
-          </th>
-          <th>
-            {" "}
-            <div className="flex--container">
-              <span>Longitude</span>
-              <BsArrowDownUp />
-            </div>
-          </th>
-          <th>
-            {" "}
-            <div className="flex--container">
-              <span>Wind Prob.</span>
-              <BsArrowDownUp />
-            </div>
-          </th>
-          <th>
-            {" "}
-            <div className="flex--container">
-              <span>When to go</span>
-              <BsArrowDownUp />
-            </div>
-          </th>
+          {tableHeadValues.map((tableHead, index) => (
+            <TableHeader
+              key={index}
+              name={tableHead.name}
+              sortKey={tableHead.sortKey}
+              sortParams={sortParams}
+              setSortParams={setSortParams}
+            />
+          ))}
         </tr>
       </thead>
       <tbody>
-        {shownSpots.map((spot) => (
+        {sortedSpots().map((spot) => (
           <tr key={spot.id}>
             <td>{spot.name}</td>
             <td>{spot.country}</td>
