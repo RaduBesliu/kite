647fcabc0a45403e881c7b204639d88a982f2ace
Radu <radu.besliu02@gmail.com>
2022-05-20 03:14:44 +0300

    style popup

--- kite/src/components/SpotMarker/SpotPopup/SpotPopup.styles.jsx
+++ kite/src/components/SpotMarker/SpotPopup/SpotPopup.styles.jsx
@@ -5,9 +5,21 @@ import SpotPopup from "./SpotPopup";
 export const StyledSpotPopup = styled(SpotPopup)`
   .leaflet-popup-content-wrapper {
     border-radius: 0;
+    background: transparent;
+    padding: 0;
   }
 
   .leaflet-popup-tip-container {
     visibility: hidden;
+  }
+
+  .leaflet-popup-close-button {
+    display: none;
+  }
+
+  background-color: red;
+  & div {
+    width: 250px;
+    height: 398px;
   }
 `;
