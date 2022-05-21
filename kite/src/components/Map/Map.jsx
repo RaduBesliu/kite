3d8ed075bedc3baf48aca19e809fa9f10ddd4764
Radu <radu.besliu02@gmail.com>
2022-05-21 03:36:48 +0300

    add test double click map event & pass props

--- kite/src/components/Map/Map.jsx
+++ kite/src/components/Map/Map.jsx
@@ -1,21 +1,52 @@
 // React
 import React from "react";
+import { useState } from "react";
 
 // Images
-import markerIconPng from "../../assets/images/marker-red.png";
+import markerIconRedPng from "../../assets/images/marker-red.png";
+import markerIconYellowPng from "../../assets/images/marker-yellow.png";
 
 // Libraries
 import { MapContainer, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import Leaflet from "leaflet";
+import { useMapEvents } from "react-leaflet/hooks";
 
 // Components
 import { StyledSpotMarker } from "../SpotMarker/SpotMarker.styles";
 
-function Map({ className, filterSpots }) {
+function Map({
+  className,
+  filterSpots,
+  favourites,
+  spots,
+  setSpots,
+  setFavourites,
+}) {
+  const [renderCount, setRenderCount] = useState(1);
+
   const corner1 = Leaflet.latLng(-90, -200);
   const corner2 = Leaflet.latLng(90, 200);
   const bounds = Leaflet.latLngBounds(corner1, corner2);
+
+  function Map() {
+    useMapEvents({
+      dblclick: (e) => {
+        console.log(e.latlng.lat, e.latlng.lng, spots);
+        setSpots([
+          ...spots,
+          {
+            favourite: false,
+            name: "test",
+            lat: e.latlng.lat,
+            long: e.latlng.lng,
+            id: 100,
+          },
+        ]);
+      },
+    });
+    return null;
+  }
 
   return (
     <MapContainer
@@ -36,19 +67,25 @@ function Map({ className, filterSpots }) {
         <StyledSpotMarker
           key={spot.id}
           position={[spot.lat, spot.long]}
+          currentSpot={spot}
+          setRenderCount={setRenderCount}
+          renderCount={renderCount}
+          favourites={favourites}
+          setFavourites={setFavourites}
           icon={
             new Icon({
-              iconUrl: markerIconPng,
+              iconUrl: spot.favourite ? markerIconYellowPng : markerIconRedPng,
               iconSize: [27, 41],
               iconAnchor: [13, 41],
               popupAnchor: [
                 spot.long < -150 ? 175 : spot.long > 150 ? -175 : 0,
-                spot.lat < -70 ? -50 : spot.lat > 70 ? 450 : -50,
+                spot.lat < -65 ? -50 : spot.lat > 65 ? 550 : -50,
               ],
             })
           }
         />
       ))}
+      <Map />
     </MapContainer>
   );
 }
