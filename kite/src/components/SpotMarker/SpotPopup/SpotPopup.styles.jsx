b22074217772ed3ad59edf3262f67a6bb6003087
Radu <radu.besliu02@gmail.com>
2022-05-21 03:39:28 +0300

    update styles + responsive mode

--- kite/src/components/SpotMarker/SpotPopup/SpotPopup.styles.jsx
+++ kite/src/components/SpotMarker/SpotPopup/SpotPopup.styles.jsx
@@ -7,6 +7,7 @@ export const StyledSpotPopup = styled(SpotPopup)`
     border-radius: 0;
     background: transparent;
     padding: 0;
+    width: 269px;
   }
 
   .leaflet-popup-tip-container {
@@ -14,12 +15,76 @@ export const StyledSpotPopup = styled(SpotPopup)`
   }
 
   .leaflet-popup-close-button {
-    display: none;
+    margin-top: 16px;
+    margin-right: 15px;
   }
 
-  background-color: red;
-  & div {
-    width: 250px;
-    height: 398px;
+  .leaflet-popup-close-button span {
+    font-size: 32px;
+    font-weight: 300;
+    color: var(--clr-gray-5);
+  }
+
+  .leaflet-popup-content {
+    margin: 0;
+  }
+
+  .leaflet-popup-content p {
+    margin: 0;
+  }
+
+  background-color: var(--clr-white-1);
+
+  & .container {
+    width: 269px;
+    height: 500px;
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.5);
+    position: relative;
+
+    & .information {
+      width: 200px;
+      height: 475px;
+      padding-left: 14px;
+      padding-top: 14px;
+
+      & h2 {
+        font-size: 26px;
+        font-weight: 700;
+      }
+
+      & h4 {
+        font-size: 20px;
+        font-weight: 400;
+        color: var(--clr-gray-6);
+        margin-bottom: 26px;
+      }
+    }
+  }
+
+  @media (max-width: 550px) {
+    .leaflet-popup-content-wrapper,
+    & .container {
+      width: 200px;
+    }
+
+    .leaflet-popup-close-button {
+      margin-right: 10px;
+    }
+
+    svg {
+      margin-left: 10px;
+    }
+
+    & .container > .information {
+      width: 170px;
+
+      & h2 {
+        font-size: 24px;
+      }
+
+      & h4 {
+        font-size: 18px;
+      }
+    }
   }
 `;
