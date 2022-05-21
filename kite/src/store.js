2696ad69d324c0a90f89f3f2f90a82d6da781ed8
Radu <radu.besliu02@gmail.com>
2022-05-21 03:42:39 +0300

    add popup reducer

--- kite/src/store.js
+++ kite/src/store.js
@@ -3,9 +3,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // Reducers
 import filterReducer from "./features/filter/filterSlice";
+import popupReducer from "./features/popup/popupSlice";
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
+    popup: popupReducer,
   },
 });
