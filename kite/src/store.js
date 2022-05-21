b9acdf3b382626970a12911f1de8ff10601e9044
Radu <radu.besliu02@gmail.com>
2022-05-21 23:01:37 +0300

    add addSpot reducer

--- kite/src/store.js
+++ kite/src/store.js
@@ -4,10 +4,12 @@ import { configureStore } from "@reduxjs/toolkit";
 // Reducers
 import filterReducer from "./features/filter/filterSlice";
 import popupReducer from "./features/popup/popupSlice";
+import addSpotSlice from "./features/addSpot/addSpotSlice";
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
     popup: popupReducer,
+    addSpot: addSpotSlice,
   },
 });
