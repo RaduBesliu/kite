e0d6726f58c8681a096728c83c431add13ceccca
Radu <radu.besliu02@gmail.com>
2022-05-21 03:38:19 +0300

    add update redux state on popup open & close + pass props

--- kite/src/components/SpotMarker/SpotMarker.jsx
+++ kite/src/components/SpotMarker/SpotMarker.jsx
@@ -2,14 +2,49 @@ import React from "react";
 
 // Libraries
 import { Marker } from "react-leaflet";
+import { useDispatch } from "react-redux";
 
 // Components
 import { StyledSpotPopup } from "./SpotPopup/SpotPopup.styles";
 
-function SpotMarker({ className, position, icon }) {
+// Features
+import { setPopupState } from "../../features/popup/popupSlice";
+
+function SpotMarker({
+  className,
+  position,
+  currentSpot,
+  setRenderCount,
+  renderCount,
+  favourites,
+  setFavourites,
+  icon,
+}) {
+  const dispatch = useDispatch();
+
+  const eventHandlers = {
+    popupopen: () => {
+      dispatch(setPopupState(true));
+    },
+    popupclose: () => {
+      dispatch(setPopupState(false));
+    },
+  };
+
   return (
-    <Marker className={className} position={position} icon={icon}>
-      <StyledSpotPopup />
+    <Marker
+      className={className}
+      position={position}
+      icon={icon}
+      eventHandlers={eventHandlers}
+    >
+      <StyledSpotPopup
+        favourites={favourites}
+        setFavourites={setFavourites}
+        currentSpot={currentSpot}
+        setRenderCount={setRenderCount}
+        renderCount={renderCount}
+      />
     </Marker>
   );
 }
