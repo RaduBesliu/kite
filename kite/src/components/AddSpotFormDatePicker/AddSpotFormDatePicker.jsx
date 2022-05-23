3ff0c43bba554724bbdcb03f80f94fe27c590617
Radu <radu.besliu02@gmail.com>
2022-05-23 18:18:51 +0300

    add date to redux store

--- kite/src/components/AddSpotFormDatePicker/AddSpotFormDatePicker.jsx
+++ kite/src/components/AddSpotFormDatePicker/AddSpotFormDatePicker.jsx
@@ -1,14 +1,46 @@
 // React
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Libraries
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+import { useSelector, useDispatch } from "react-redux";
+
+// Features
+import { setNewSpot } from "../../features/addSpot/addSpotSlice";
+
 function AddSpotFormDatePicker() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+
+  const monthsOrder = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December",
+  };
+
+  const dispatch = useDispatch();
+
+  const addSpot = useSelector((state) => state.addSpot.value);
+
+  useEffect(() => {
+    console.log(monthsOrder[startDate.getMonth() + 1]);
+    dispatch(
+      setNewSpot({ ...addSpot, month: monthsOrder[startDate.getMonth() + 1] })
+    );
+  }, [startDate]);
+
   return (
     <div className={"date--flex"}>
       <DatePicker
