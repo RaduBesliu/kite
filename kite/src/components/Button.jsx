97a3d20961124ee1fb4c6f85fe07615a13e60bb1
Radu <radu.besliu02@gmail.com>
2022-05-23 23:46:35 +0300

    add disabled property

--- kite/src/components/Button.jsx
+++ kite/src/components/Button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Button({ title, handleClick, className }) {
-  return (
+function Button({ title, handleClick, className, disabled }) {
+  return disabled === true ? (
+    <button
+      className={className}
+      onClick={handleClick}
+      disabled
+      style={{ backgroundColor: "var(--clr-gray-6)" }}
+    >
+      {title}
+    </button>
+  ) : (
     <button className={className} onClick={handleClick}>
       {title}
     </button>
