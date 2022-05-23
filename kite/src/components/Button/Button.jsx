b41c7c939ab81f0cf3a48fa6d2dd8c1d8bf9f343
Radu <radu.besliu02@gmail.com>
2022-05-23 23:30:15 +0300

    add disabled state to button

--- kite/src/components/Button/Button.jsx
+++ kite/src/components/Button/Button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Button({ className, buttonLabel, handleClick }) {
-  return (
+function Button({ className, buttonLabel, handleClick, disabled }) {
+  return disabled === true ? (
+    <button
+      className={className}
+      onClick={handleClick}
+      disabled
+      style={{ backgroundColor: "var(--clr-gray-6)" }}
+    >
+      {buttonLabel}
+    </button>
+  ) : (
     <button className={className} onClick={handleClick}>
       {buttonLabel}
     </button>
